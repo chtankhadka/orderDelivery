8d3681875607078aa566610e315daafc8a590dac
chtan <chetan.dtech@gmail.com>
2023-09-15 14:50:51 +0545

    init

---
+++ app/src/main/java/com/chetan/orderdelivery/OrderDeliveryApp.kt
@@ -0,0 +1,7 @@
+package com.chetan.orderdelivery
+
+import android.app.Application
+
+
+class OrderDeliveryApp : Application() {
+}
---
+++ app/src/main/java/com/chetan/orderdelivery/di/FirebaseModule.kt
@@ -0,0 +1,19 @@
+package com.chetan.orderdelivery.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore{
+        return FirebaseFirestore.getInstance()
+    }
+}
